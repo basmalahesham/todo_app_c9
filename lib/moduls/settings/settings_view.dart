308b0c73132e7983b931429b0d59fc71de7c1435
basmalahesham <112768821+basmalahesham@users.noreply.github.com>
2023-08-23 11:45:00 +0300

    create todoApp

--- lib/moduls/settings/settings_view.dart
+++ lib/moduls/settings/settings_view.dart
@@ -1,10 +1,132 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+import '../../core/theme/app_theme.dart';
 
 class SettingsView extends StatelessWidget {
   const SettingsView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    var mediaQuery = MediaQuery.of(context).size;
+    return Column(
+      children: [
+        Container(
+          alignment: Alignment.centerLeft,
+          padding: EdgeInsets.only(top: 40, left: 20),
+          width: mediaQuery.width,
+          height: mediaQuery.height * 0.15,
+          color: AppTheme.primaryColor,
+          child: Text(
+            'Settings',
+            style: GoogleFonts.poppins(
+              fontWeight: FontWeight.bold,
+              fontSize: 22,
+              color: Colors.white,
+            ),
+          ),
+        ),
+        Center(
+          child: Container(
+            padding: EdgeInsets.symmetric(
+              horizontal: 20,
+              vertical: 35,
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                Text(
+                  'Language',
+                  textAlign: TextAlign.start,
+                  style: GoogleFonts.poppins(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 14,
+                    color: Colors.black,
+                  ),
+                ),
+                InkWell(
+                  onTap: () {},
+                  child: Container(
+                    margin: const EdgeInsets.only(
+                      top: 20,
+                      left: 10,
+                      right: 10,
+                    ),
+                    padding: const EdgeInsets.symmetric(
+                        horizontal: 12, vertical: 10),
+                    height: 55,
+                    decoration: BoxDecoration(
+                        color: Colors.white,
+                        border: Border.all(color: AppTheme.primaryColor)),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(
+                          'English',
+                          style: GoogleFonts.roboto(
+                            fontWeight: FontWeight.normal,
+                            fontSize: 14,
+                            color: AppTheme.primaryColor,
+                          ),
+                        ),
+                        Icon(
+                          Icons.keyboard_arrow_down_outlined,
+                          color: AppTheme.primaryColor,
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                const SizedBox(
+                  height: 30,
+                ),
+                Text(
+                  'Mode',
+                  textAlign: TextAlign.start,
+                  style: GoogleFonts.poppins(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 14,
+                    color: Colors.black,
+                  ),
+                ),
+                InkWell(
+                  onTap: () {},
+                  child: Container(
+                    margin: const EdgeInsets.only(
+                      top: 20,
+                      left: 10,
+                      right: 10,
+                    ),
+                    padding: const EdgeInsets.symmetric(
+                        horizontal: 12, vertical: 10),
+                    height: 55,
+                    decoration: BoxDecoration(
+                        color: Colors.white,
+                        border: Border.all(color: AppTheme.primaryColor)),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(
+                          'Light',
+                          style: GoogleFonts.roboto(
+                            fontWeight: FontWeight.normal,
+                            fontSize: 14,
+                            color: AppTheme.primaryColor,
+                          ),
+                        ),
+                        Icon(
+                          Icons.keyboard_arrow_down_outlined,
+                          color: AppTheme.primaryColor,
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        )
+      ],
+    );
   }
 }
